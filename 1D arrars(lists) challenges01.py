ae228e21804910b00cfccdf756ed418978d63b4d
stefanb78 <78418144+stefanb78@users.noreply.github.com>
2021-05-05 10:16:31 +0100

    Create 1D arrars(lists) challenges01.py

---
+++ 1D arrars(lists) challenges01.py
@@ -0,0 +1,6 @@
+cars = []
+carsadd = input("please enter a car brand name: \n")
+cars.append(carsadd)
+carsadd2 = input("please enter two more car brand names: \n")
+cars.append(carsadd2)
+print(cars)
