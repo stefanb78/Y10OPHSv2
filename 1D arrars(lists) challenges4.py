258ae9f56bdb391a34512c9e0daeea68f026f788
stefanb78 <78418144+stefanb78@users.noreply.github.com>
2021-05-18 11:33:15 +0100

    Create 1D arrars(lists) challenges4.py

---
+++ 1D arrars(lists) challenges4.py
@@ -0,0 +1,12 @@
+TvShows=["Attack on Titan", "Breaking Bad", "Neon Genesis Evangelion", "Peppa Pig"]
+
+for shows in TvShows:
+      print(shows)
+
+addshow=str(input("Enter another show: \n"))
+
+TvShows.append(addshow)
+
+for shows in TvShows:
+      print(shows)
+      
